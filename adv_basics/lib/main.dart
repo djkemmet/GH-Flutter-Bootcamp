79e10400175f54b95aac2d97cd0049d17e3690c8
DJ Kemmet <dj_kemmet@carmax.com>
2024-03-04 12:06:55 -0600

    S3V52 - Complete
    Scaffolded the challenge app.

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -12,8 +12,35 @@ class advBasicsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.purple[700],
         body: Center(
-      child: Text("This is Text"),
-    ));
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'images/quiz-logo.png',
+                scale: 2,
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24
+                ),
+              ),
+              const SizedBox(height: 64),
+              TextButton(
+                child: Text(
+                  'Start Quiz',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ));
   }
 }
