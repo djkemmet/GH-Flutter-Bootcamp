3c8d8ebb8d7ff76777cdbcaf47c52a0c10713535
DJ Kemmet <dj_kemmet@carmax.com>
2024-03-04 14:45:32 -0600

    S2V54 Complete
    App is now more or less in line with the course, and I can continue with all the new stuff

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -4,10 +4,16 @@ import 'start_screen.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.purple[700],
-      body: Center(child:StartScreenView()),
-
+      backgroundColor: Colors.deepPurple,
+      body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168)
+            ],
+          )),
+          child: Center(child: StartScreenView())),
     ),
   ));
 }
-
