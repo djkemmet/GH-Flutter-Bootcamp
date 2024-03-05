638e9e5904fd6267b3742bdfaf0ae17723a11cb2
Daniel Kemmet <dj_kemmet@carmax.com>
2024-03-05 12:49:41 -0600

    S3V67 - Configuring a Column
    Introduced mainAxisAlignment

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,24 +1,32 @@
 // needs to be a stateful widget
 import 'package:flutter/material.dart';
 
-
 class QuestionsScreenView extends StatefulWidget {
   const QuestionsScreenView({super.key});
-  
+
   @override
   State<QuestionsScreenView> createState() {
     return _QuestionsScreenViewState();
   }
 }
 
-
 class _QuestionsScreenViewState extends State<QuestionsScreenView> {
-  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text("This Is A Stateful Container for the Questions Screen"),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text("The Question Text"),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+        const SizedBox(height:12),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
+        const SizedBox(height:12),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+        const SizedBox(height:12),
+        ElevatedButton(onPressed: () {}, child: const Text('Answer 4')),
+        const SizedBox(height:12),
+      ],
     );
   }
-
 }
