71ccfc988e574fde6a247c98ebe7f7bec85ead9d
Daniel Kemmet <dj_kemmet@carmax.com>
2024-04-03 13:14:34 -0500

    S5V112. Closing The Modal Manually

--- expense_tracker/expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/expense_tracker/lib/widgets/new_expense.dart
@@ -1,47 +1,56 @@
 import 'package:flutter/material.dart';
 
-class NewExpense extends StatefulWidget{
+class NewExpense extends StatefulWidget {
   NewExpense({super.key});
 
   @override
-  State<NewExpense> createState(){
+  State<NewExpense> createState() {
     return _NewExpenseState();
   }
 }
 
 class _NewExpenseState extends State<NewExpense> {
-
-final _titleController  = TextEditingController();
-final _amountController = TextEditingController();
-
-@override
-void dispose(){
-  _titleController.dispose();
-  super.dispose();
-}
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
-  Widget build(BuildContext context){
-    return  Padding(
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          TextField(
-            controller: _titleController,
-            maxLength: 50,
-            decoration: const InputDecoration(label:Text("Expense Name")),
+      child: Column(children: [
+        TextField(
+          controller: _titleController,
+          maxLength: 50,
+          decoration: const InputDecoration(label: Text("Expense Name")),
+        ),
+        TextField(
+          keyboardType: const TextInputType.numberWithOptions(),
+          controller: _amountController,
+          decoration: const InputDecoration(
+            label: Text("Amount"),
+            prefixText: '\$',
           ),
-          TextField(
-            keyboardType: const TextInputType.numberWithOptions(),
-            controller: _amountController,
-            decoration:  const InputDecoration(label: const Text("Amount"), prefixText:'\$',),
-          ),
-          Row(children:[
-            ElevatedButton(onPressed: (){print(_titleController.text);print(_amountController.text);}, child: const Text("Save Expense")),
-            ElevatedButton(onPressed: (){Navigator.of(context).pop();}, child: const Text("Close"))
-          ])
-        ]
-      ),
+        ),
+        Row(children: [
+          ElevatedButton(
+              onPressed: () {
+                print(_titleController.text);
+                print(_amountController.text);
+              },
+              child: const Text("Save Expense")),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Close"))
+        ])
+      ]),
     );
   }
 }
