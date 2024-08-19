782d6806c22cbea2713d75b399f596b8ddc8d2d4
DJ Kemmet <kemmetdaniel@gmail.com>
2024-08-19 18:07:53 -0500

    S8V160 - Loading Meals Data Into a Screen

--- meals_app/meals/lib/screens/meals.dart
+++ meals_app/meals/lib/screens/meals.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:meals/models/meal.dart';
+import 'package:meals/data/dummy_data.dart';
 
 class MealsScreen extends StatelessWidget {
   // Class Constructor
@@ -13,9 +13,36 @@ class MealsScreen extends StatelessWidget {
   // Class Build method
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (context, integer) => Text(dummyMeals[integer].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Uh oh ... nothing here!",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(color: Colors.white)),
+            const SizedBox(height: 16),
+            Text(
+              "Try selecting a different Category",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Colors.white),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Container(),
+      body: Scaffold(appBar: AppBar(title: Text(title)), body: content),
     );
   }
 }
