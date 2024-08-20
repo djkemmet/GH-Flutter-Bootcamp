219b0ae5f2704f8a428bbe9ddc66f77e6025cb29
DJ Kemmet <kemmetdaniel@gmail.com>
2024-08-19 19:38:40 -0500

    S8V163- Introducing the Stack Widget

--- meals_app/meals/lib/screens/meals.dart
+++ meals_app/meals/lib/screens/meals.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/data/dummy_data.dart';
 
+// Widget Imports
+import 'package:meals/widgets/meal_item.dart';
+
 class MealsScreen extends StatelessWidget {
   // Class Constructor
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -15,7 +18,7 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length,
-      itemBuilder: (context, integer) => Text(dummyMeals[integer].title),
+      itemBuilder: (context, integer) => MealItem(meal: meals[integer]),
     );
 
     if (meals.isEmpty) {
