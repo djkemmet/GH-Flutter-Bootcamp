56d03f10d4bee645ea438d874851935d3d4d254f
DJ Kemmet <kemmetdaniel@gmail.com>
2024-08-21 19:12:11 -0500

    S8V166 - Improving the MealDetails Screen

--- meals_app/meals/lib/screens/meal_detail.dart
+++ meals_app/meals/lib/screens/meal_detail.dart
@@ -13,10 +13,44 @@ class MealsDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(meal.title),
+        title: Text(meal.title,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary)),
       ),
-      body: Image.network(meal.imageUrl,
-          height: 300, width: double.infinity, fit: BoxFit.cover),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Image.network(meal.imageUrl,
+              height: 300, width: double.infinity, fit: BoxFit.cover),
+          const SizedBox(height: 14),
+          Text(
+            "Ingredients",
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(color: Colors.white),
+          ),
+          const SizedBox(height: 14),
+          for (final ingredient in meal.ingredients)
+            Text(ingredient, style: const TextStyle(color: Colors.white)),
+          const SizedBox(height: 24),
+          Text(
+            "Steps",
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(color: Colors.white),
+          ),
+          const SizedBox(height: 14),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              child: Text(step,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white)),
+            ),
+        ]),
+      ),
     );
   } // Ends Build Method.
 } // Ends Screen class
