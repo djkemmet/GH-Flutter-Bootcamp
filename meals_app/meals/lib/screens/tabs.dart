e7a83ca938238ed51a5067b81c48984da00e9898
DJ Kemmet <kemmetdaniel@gmail.com>
2024-08-24 19:36:42 -0500

    S9V1185 - Creating a Provider

--- meals_app/meals/lib/screens/tabs.dart
+++ meals_app/meals/lib/screens/tabs.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-
-import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
+
+//Provider Imports
+import 'package:meals/providers/meals_provider.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
@@ -78,7 +79,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final availableMeals = dummyMeals.where((meal) {
+    final availableMeals = mealsProvider.where((meal) {
       if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
         return false;
       }
